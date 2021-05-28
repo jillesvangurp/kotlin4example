dd71c33644298791a75503230d87736566821d20
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2021-05-28 09:24:36 +0200

    tweak text

--- src/test/kotlin/com/jillesvangurp/kotlin4example/docs/readme.kt
+++ src/test/kotlin/com/jillesvangurp/kotlin4example/docs/readme.kt
@@ -5,6 +5,8 @@ import com.jillesvangurp.kotlin4example.SourceRepository
 
 val k4ERepo = SourceRepository("https://github.com/jillesvangurp/kotlin4example")
 
+// You can still use comment markers to grab larger sections of code
+// or to grab code from e.g. the main source tree.
 // READMECODESTART
 val readme by k4ERepo.md {
     // for larger bits of text, it's nice to load them from a markdown file
@@ -19,7 +21,9 @@ val readme by k4ERepo.md {
                 println("Hello World")
             }
         }
-        // but of course you can inline a Kotlin multiline string with some markdown
+        // of course you can inline a Kotlin multiline string with some markdown
+        // note the use of templating here and the helper function to generate
+        // a link
         +"""
             Here's the same block as above running as part of this 
             ${mdLinkToSelf("readme.kt")} file.
@@ -31,13 +35,13 @@ val readme by k4ERepo.md {
 
         +"""
             As you can see, we indeed show a pretty printed block, ran it, and
-            grabbed the output. Observant readers will also note that the nested 
+            grabbed the output as well. Observant readers will also note that the nested 
             block above did not run. The reason for this is that the outer `block` 
-            call for that has a parameter that you can use to prevent this. 
+            call for that has a `runBlock` parameter that you can use to prevent this. 
             If you look at the source code for the readme, you will see we used 
             `block(runBlock = false)`
             
-            We can also return a value from the block and capture that:
+            You can also return a value from the block and capture that:
         """
 
         block {
@@ -61,7 +65,8 @@ val readme by k4ERepo.md {
             source code that generates this markdown ${mdLinkToSelf("here")}.
         """.trimIndent()
 
-        // little hack so it will read until the end marker
+        // little string concatenation hack so it will read 
+        // until the end marker instead of stopping here        
         snippetFromSourceFile(
             "com/jillesvangurp/kotlin4example/docs/readme.kt",
             "README" + "CODE"
