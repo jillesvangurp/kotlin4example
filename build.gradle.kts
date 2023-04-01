763b85a1aa2aaf5f3683fff949a1db6a1807f0bd
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-04-01 05:35:52 +0200

    upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,10 +31,10 @@ dependencies {
 
 tasks {
     compileKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
     compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 }
 
